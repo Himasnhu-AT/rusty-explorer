9238b072fcfa9b4b933c8f7280cea6571ddd1208
Himanshu-AT <aristech.official@gmail.com>
2023-11-26 19:05:50 +0530

    beta release

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+mod filesystem;
+mod search;
+mod errors;
+
+use filesystem::explorer::{open_file, open_directory, create_file, create_directory, rename_file, delete_file};
+use filesystem::volume::get_volumes;
+use search::search_directory;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+#[derive(Serialize, Deserialize)]
+pub struct CachedPath {
+    #[serde(rename = "p")]
+    file_path: String,
+    #[serde(rename = "t")]
+    file_type: String,
 }
 
-fn main() {
+pub type VolumeCache = HashMap<String, Vec<CachedPath>>;
+
+#[derive(Default)]
+pub struct AppState {
+    system_cache: HashMap<String, VolumeCache>,
+}
+
+pub type StateSafe = Arc<Mutex<AppState>>;
+
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_volumes,
+            open_directory,
+            search_directory,
+            open_file,
+            create_file,
+            create_directory,
+            rename_file,
+            delete_file
+        ])
+        .manage(Arc::new(Mutex::new(AppState::default())))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,139 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import {useEffect, useState} from "react";
+import {invoke} from "@tauri-apps/api/tauri";
+import {DirectoryContent, Volume} from "./types";
+import {openDirectory} from "./ipc";
+import VolumeList from "./components/MainBody/Volumes/VolumeList";
+import FolderNavigation from "./components/TopBar/FolderNavigation";
+import {DirectoryContents} from "./components/MainBody/DirectoryContents";
+import useNavigation from "./hooks/useNavigation";
+import SearchBar from "./components/TopBar/SearchBar";
+import {useAppDispatch, useAppSelector} from "./state/hooks";
+import useContextMenu from "./hooks/useContextMenu";
+import ContextMenus from "./components/ContextMenus/ContextMenus";
+import {
+  selectDirectoryContents,
+  unselectDirectoryContents,
+  updateDirectoryContents
+} from "./state/slices/currentDirectorySlice";
+import {DIRECTORY_ENTITY_ID} from "./components/MainBody/DirectoryEntity";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [volumes, setVolumes] = useState<Volume[]>([]);
+  const directoryContents = useAppSelector(selectDirectoryContents);
+  const dispatch = useAppDispatch();
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const [searchResults, setSearchResults] = useState<DirectoryContent[]>([]);
+
+  const {
+    pathHistory,
+    historyPlace,
+    setHistoryPlace,
+    onBackArrowClick,
+    onForwardArrowClick,
+    canGoBackward,
+    canGoForward,
+    currentVolume,
+    setCurrentVolume,
+  } = useNavigation(searchResults, setSearchResults);
+
+  async function getNewDirectoryContents() {
+    const contents = await openDirectory(pathHistory[historyPlace]);
+    dispatch(updateDirectoryContents(contents));
   }
 
+  async function onVolumeClick(mountpoint: string) {
+    if (pathHistory[pathHistory.length - 1] != mountpoint) {
+      pathHistory.push(mountpoint);
+    }
+    setHistoryPlace(pathHistory.length - 1);
+    setCurrentVolume(mountpoint);
+
+    await getNewDirectoryContents();
+  }
+
+  async function onDirectoryClick(filePath: string) {
+    if (searchResults.length > 0) {
+      setSearchResults([]);
+    }
+
+    pathHistory.push(filePath);
+    setHistoryPlace(pathHistory.length - 1);
+
+    await getNewDirectoryContents();
+  }
+
+  async function getVolumes() {
+    if (volumes.length > 0) {
+      return;
+    }
+
+    const newVolumes = await invoke<Volume[]>("get_volumes");
+    setVolumes(newVolumes);
+  }
+
+  let render = 0;
+
+  useEffect(() => {
+    if (render === 0) {
+      getVolumes().catch(console.error);
+    }
+
+    render += 1; // I don't know why but the use effect runs twice causing the "get_volumes" to be called twice.
+  }, [])
+
+  useEffect(() => {
+    if (pathHistory[historyPlace] == "") {
+      setCurrentVolume("");
+      return;
+    }
+
+    getNewDirectoryContents().catch(console.error);
+  }, [historyPlace]);
+
+  const [handleMainContextMenu, handleCloseContextMenu] = useContextMenu(dispatch, pathHistory[historyPlace]);
+
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="h-full" onClick={(e) => {
+      handleCloseContextMenu(e);
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      if (e.target instanceof HTMLElement) {
+        if (e.target.id === DIRECTORY_ENTITY_ID) return;
+      }
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      dispatch(unselectDirectoryContents());
+    }} onContextMenu={handleMainContextMenu}>
+      <ContextMenus />
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+      <div className="p-4">
+        <FolderNavigation
+            onBackArrowClick={onBackArrowClick}
+            canGoBackward={canGoBackward()}
+            onForwardArrowClick={onForwardArrowClick}
+            canGoForward={canGoForward()}
         />
-        <button type="submit">Greet</button>
-      </form>
 
-      <p>{greetMsg}</p>
+        <div className="pb-5">
+          <SearchBar
+              currentVolume={currentVolume}
+              currentDirectoryPath={pathHistory[historyPlace]}
+              setSearchResults={setSearchResults}
+          />
+
+          <div className="w-7/12">
+            {pathHistory[historyPlace] === "" && searchResults.length === 0 ? (
+                <VolumeList volumes={volumes} onClick={onVolumeClick} />
+            ) : (
+                <DirectoryContents
+                    content={
+                      searchResults.length === 0 ? directoryContents : searchResults
+                    }
+                    onDirectoryClick={onDirectoryClick}
+                />
+            )}
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
